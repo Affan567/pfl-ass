f10e4de0a844831c5cf7edcdf1399a627662211d
Affan567 <m.affanasim515@gmail.com>
2024-09-15 21:48:37 +0500

    Add files via upload

---
+++ sixth.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	cout<<"               square            "<<endl;
+
+	cout<<"             ##########          "<<endl;
+	cout<<"             #        #          "<<endl;
+	cout<<"             #        #          "<<endl;
+	cout<<"             #        #          "<<endl;
+	cout<<"             ##########          "<<endl;
+
+
+	cout<<"              Triangle           "<<endl;
+
+	cout<<"			*               "<<endl;
+	cout<<"		      *   *             "<<endl;
+	cout<<"		     *     *            "<<endl;
+	cout<<"		    *       *           "<<endl;
+	cout<<"		   *  *  * * *          "<<endl;
+	
+
+
+	cout<<"		       circle           "<<endl;
+
+	cout<<"		        :::::           "<<endl;
+	cout<<"             :::      :::        "<<endl;
+	cout<<"            ::           ::      "<<endl;
+	cout<<"            :             :      "<<endl;
+	cout<<"            :             :      "<<endl;
+	cout<<"             ::         ::       "<<endl;
+	cout<<"               :::    :::        "<<endl;
+	cout<<"	       	         ::::           "<<endl;
+
+
+
+	cout<<"	             Parallelogram      "<<endl;
+
+	cout<<"	             $$$$$$$$$$$        "<<endl;
+	cout<<"	            $         $         "<<endl;
+	cout<<"	           $         $          "<<endl;
+	cout<<"	          $$$$$$$$$$$           "<<endl;
+
+
+
+	cout<<"                Hexagon          "<<endl;
+
+	cout<<"		       *******          "<<endl;
+	cout<<"		      *       *         "<<endl;
+	cout<<"		     *         *        "<<endl;
+	cout<<"		      *       *         "<<endl;
+	cout<<"		       *******          "<<endl;
+
+
+}
